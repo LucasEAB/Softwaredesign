423feabbe0dd12cf12527b516e2a1b701a076431
LucasEAB <lucaseab@hotmail.de>
2018-04-01 16:38:11 +0200

    Aufgaben

---
+++ Aufgabe1.1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Aufgabe1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = args[1];
+            double d = Convert.ToDouble(b);
+
+            switch (args[0])
+            {
+                case "c":
+                    getCubeInfo(d);
+                    break;
+                case "k":
+                    getSphereInfo(d);
+                    break;
+                case "o":
+                    getOctahedronInfo(d);
+                    break;
+
+                default:
+                    Console.WriteLine("Wähle c, k oder o");
+                    break;
+            }
+        }
+
+        //Cube
+        public static double getCubeSurface(double Durchmesser)
+        {
+            double d = Durchmesser;
+            double A = Math.Round(6 * (d * d), 2);
+            return A;
+        }
+        public static double getCubeVolume(double Durchmesser)
+        {
+            double d = Durchmesser;
+            double V = Math.Round(d * d * d, 2);
+            return V;
+        }
+        public static void getCubeInfo(double Durchmesser)
+        {
+            Console.WriteLine("Würfel:  A = " + getCubeSurface(Durchmesser) + "  |  V = " + getCubeVolume(Durchmesser));
+        }
+
+        //Kugel 
+        public static double getSphereSurface(double Durchmesser)
+        {
+            double d = Durchmesser;
+            double A = Math.Round(Math.PI * (d * d), 2);
+            return A;
+        }
+        public static double getSphereVolume(double Durchmesser)
+        {
+            double d = Durchmesser;
+            double V = Math.Round((Math.PI * (d * d * d)) / 6,2);
+            return V;
+        }
+        public static void getSphereInfo(double Durchmesser)
+        {
+            Console.WriteLine("Kugel:  A = " + getSphereSurface(Durchmesser) + "  |  V = " + getSphereVolume(Durchmesser));
+        }
+
+        //Oktaeder
+        public static double getOctahedronSurface(double Durchmesser)
+        {
+            double d = Durchmesser;
+            double A = Math.Round(2 * Math.Sqrt(3) * (d * d), 2);
+            return A;
+        }
+        public static double getOctahedronVolume(double Durchmesser)
+        {
+            double d = Durchmesser;
+            double V = Math.Round(Math.Sqrt(2) * (d * d * d) / 3, 2);
+            return V;
+        }
+        public static void getOctahedronInfo(double Durchmesser)
+        {
+            Console.WriteLine("Oktaeder:  A = " + getOctahedronSurface(Durchmesser) + "  |  V = " + getOctahedronVolume(Durchmesser));
+        }
+    }
+}
---
+++ Aufgabe1.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Aufgabe1._2
+{
+    class Program
+    {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        
+        static int l = subjects.Length;
+        static string w1;
+        static string w2;
+        static string w3;
+        static void Main(string[] args)
+        {
+            //Ausgabe
+            string[] verse = new string[l];
+            for (int i = 0; i < l; i++)
+            {
+                GetVerse();
+                verse[i] = w1 + " " + w2 + " " + w3;
+            }
+            for (int v = 0; v < l; v++)
+            {
+                Console.WriteLine(verse[v]);
+            }
+        }
+
+        public static void GetVerse()
+        {
+            Random rnd = new Random();
+
+            int s = rnd.Next(0, l); // Zahl Subjekt
+            int v = rnd.Next(0, l); // Zahl verb 
+            int o = rnd.Next(0, l); // Zahl Objekt
+
+            while (subjects[s] == "benutzt")
+            {
+                s = rnd.Next(0, l);
+            }
+            while (verbs[v] == "benutzt")
+            {
+                v = rnd.Next(0, l);
+            }
+            while (objects[o] == "benutzt")
+            {
+                o = rnd.Next(0, l);
+            }
+            w1 = subjects[s];
+            w2 = verbs[v];
+            w3 = objects[o];
+
+            subjects[s] = "benutzt";
+            verbs[v] = "benutzt";
+            objects[o] = "benutzt";
+        }
+    }
+}
