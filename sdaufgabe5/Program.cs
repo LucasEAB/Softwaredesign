b168cdd4d790ad3b39335116c42e856d0c7a1190
lucaseab <lucaseab@hotmail.de>
2018-05-28 02:27:10 +0200

    aufg5

---
+++ sdaufgabe5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public List<Participants<P>> ParticipantsList = new List<ParticipantsList<P>>();
+
+        public List<Course<C>> CourseList = new List<CourseList<C>>();
+
+        public class Person
+        {
+            public string LastName;
+            public string FirstName;
+            public int age;
+        }
+        public class Participants : Person
+        {
+            public int MatriculationNumber;
+            public CourseList<C> Course; 
+        }
+        public class Lecturer : Person
+        {
+            public string Room;
+            public DateTime Hours;
+            public CourseList<C> Course; 
+
+            public void AddCourse(CourseList<C> course)
+        {
+            CourseList.Add(course);
+        }
+            public void AddParticipants(ParticipantsList<P> participants)
+        {
+            ParticipantsList.Add(participants);
+        } 
+        }
+        public class Course
+        {
+            public string Title;
+            public DateTime Date;
+            public DateTime Time;
+            public string Room;
+
+            public string outputInformationText()
+            {
+                return ;
+            }
+        }
+    }
+}
